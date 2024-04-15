404af9cfd5e4ee90a6e3e531460cb129ef47e3c9
ZvonimirZlo <lovrez2@gmail.com>
2024-04-15 20:43:27 +0200

    comments added

--- react-weather-app/src/App.jsx
+++ react-weather-app/src/App.jsx
@@ -20,7 +20,7 @@ function App() {
   const [bg, setBg] = useState(thunderstorm);
   const [description, setDescription] = useState('');
 
-  //fetching api
+  //fetch api
   const getWeatherData = () => {
     fetch(`https://api.open-meteo.com/v1/forecast?latitude=${selectedCity?.value?.latitude}&longitude=${selectedCity?.value?.longitude}&current=temperature_2m,relative_humidity_2m,apparent_temperature,is_day,weather_code,rain,cloud_cover,surface_pressure,wind_speed_10m&hourly=temperature_2m&daily=weather_code,temperature_2m_max,temperature_2m_min,sunrise,sunset,daylight_duration,uv_index_max&timezone=GMT`)
       .then(res => res.json())
@@ -54,7 +54,7 @@ function App() {
   };
 
 
-  //changing background image depending on WMO code
+  //changes background image and weather description depending on WMO code
   useEffect(() => {
     const changeBg = () => {
       let code = weatherData?.current?.weather_code;
@@ -101,6 +101,7 @@ function App() {
     <>
       <div className="app" style={{ backgroundImage: `url(${bg})` }}>
         <div className='container'>
+          {/* select country */}
           <Select
             styles={{
               control: (baseStyles, state) => ({
@@ -115,7 +116,7 @@ function App() {
             value={selectedCountry}
             onChange={handleSelectedCountry}
           />
-
+          {/* select city */}
           <Select
             styles={{
               control: (baseStyles, state) => ({
@@ -142,7 +143,7 @@ function App() {
 
           {/* on click rendering weather data on the screen */}
           <button onClick={getWeatherData}>Get Weather</button>
-
+          {/* weather data */}
           <div className="data">
             {weatherData ? (
               <div>
@@ -163,15 +164,15 @@ function App() {
             ) : (false)}
           </div>
         </div>
-      
-      <footer className="footer">©2024 Made by ZvonimirZlo
-        <a href={`https://github.com/ZvonimirZlo`} rel='noopener noreferer'>
-          <svg xmlns="http://www.w3.org/2000/svg" width="40" height="25" fill="white" viewBox="0 0 16 16">
-            <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8" />
-          </svg>
-        </a></footer>
-        </div>
-  </>
+        {/* footer */}
+        <footer className="footer">©2024 Made by ZvonimirZlo
+          <a href={`https://github.com/ZvonimirZlo`} rel='noopener noreferer'>
+            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="25" fill="white" viewBox="0 0 16 16">
+              <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8" />
+            </svg>
+          </a></footer>
+      </div>
+    </>
   )
 
 }
